977159f2273ad9f7cd049f705468580de0ced301
Alexis Bellido <alexis@ventanazul.com>
2020-05-07 13:21:19 -0400

    require example

---
+++ nodejs.dev/animals.js
@@ -0,0 +1,16 @@
+const dog = {
+  category: 'mammal',
+  friendly: true,
+  legs: 4,
+}
+
+exports.dog = dog
+
+const parrot = {
+    category: 'bird',
+    friendly: false,
+    legs: 2,
+}
+  
+exports.dog = dog
+exports.parrot = parrot
---
+++ nodejs.dev/require-examples.js
@@ -0,0 +1,6 @@
+const car = require('./car')
+console.log(car);
+
+const animals = require('./animals')
+console.log(animals.dog);
+console.log(animals.parrot);
