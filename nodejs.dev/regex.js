10bca756716455bf16e18d24d14a80794a65685c
Alexis Bellido <alexis@ventanazul.com>
2020-05-13 19:04:36 -0400

    start with regexes

--- nodejs.dev/regex.js
+++ nodejs.dev/regex.js
@@ -4,4 +4,21 @@ const parseArgs = require('minimist');
 
 const args = parseArgs(process.argv.slice(2));
 // console.log(args);
-console.log(`Needle: '${args.needle}'. Haystack: '${args.haystack}'.`);
+console.log(`Needle: '${args.needle}'. Haystack: '${args.haystack}'`);
+
+// regular expression literal provides compilation of the regular expression
+// when the script is loaded. If the regular expression remains constant, using this can improve performance.
+let re = /ab+c/;
+console.log(re);
+
+// calling the constructor function of the RegExp object
+// provides runtime compilation of the
+// regular expression. Use the constructor function when you know
+// the regular expression pattern will be changing, or you don't know
+// the pattern and are getting it from another source, such as user input.
+// let re2 = new RegExp('ab+c');
+// console.log(re2);
+
+
+
+
