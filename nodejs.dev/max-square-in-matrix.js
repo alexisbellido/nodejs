7f6ba6a3121153730a296b749d074b1652ccac4d
Alexis Bellido <alexis@ventanazul.com>
2020-05-18 10:36:04 -0400

    max square

--- nodejs.dev/max-square-in-matrix.js
+++ nodejs.dev/max-square-in-matrix.js
@@ -22,12 +22,59 @@ const find_max_square_1 = matrix => {
 
 }
 
+const findMaxSquare = (matrix) => {
+    // const cache = [...matrix];
+    const cache = [];
+    let result = 0;
+    const rows = matrix.length;
+    const cols = matrix[0].length;
+    
+    for (i = 0 ; i < rows; i++) {
+        // spread can simplify cloning making a shallow copy of each row
+        cache.push([...matrix[i]]);
+        // this would be copying each value per column
+        // cache.push([]);
+        // for (j = 0; j < cols; j++) {
+        //     cache[i].push(matrix[i][j]);
+        // }
+    }
+
+    for (i = 0 ; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            if (i == 0 || j == 0) {
+                // do nothing for top row or left column
+                // cache[i][j] = matrix[i][j];
+            } else if (matrix[i][j] > 0) {
+                cache[i][j] = 1 + Math.min(...[
+                    cache[i - 1][j],
+                    cache[i][j - 1],
+                    cache[i - 1][j - 1],
+                ]);
+            }
+            if (cache[i][j] > result) {
+                result = cache[i][j];
+            }
+        }
+    }
+    console.log('cache');
+    console.log(cache);
+    return result;
+
+};
+
 const matrix = [
-    [1, 0, 1, 0, 0],
-    [1, 0, 1, 1, 1],
+    [1, 1, 1, 1, 0],
     [1, 1, 1, 1, 1],
-    [1, 0, 0, 1, 0],
+    [1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 0],
 ];
+
+// const matrix = [
+//     [1, 0, 1, 0, 0],
+//     [1, 0, 1, 1, 1],
+//     [1, 1, 1, 1, 1],
+//     [1, 0, 0, 1, 0],
+// ];
 
 // const matrix = [
 //     [1, 1],
@@ -35,6 +82,16 @@ const matrix = [
 //     [1, 1],
 // ];
 
-find_max_square_1(matrix);
+console.log('matrix');
+console.log(matrix);
+
+const result = findMaxSquare(matrix);
+console.log('result', result);
+
+// Math.min and Math.max expect numbers but I can spread an array and pass it
+// const nums = [1, 2, 3]
+// console.log(Math.min(...nums));
+// console.log(Math.max(...nums));
+
 
 
